4a272a1d431fbf1677be93fb74b70c11c5000c1e
FHao233 <610284360@qq.com>
2023-03-19 13:50:15 +0800

    用户登录功能

---
+++ mybilibili-service/src/main/java/com/fhao/util/TokenUtil.java
@@ -0,0 +1,14 @@
+package com.fhao.util;
+
+/**
+ * author: FHao
+ * create time: 2023-03-19 13:47
+ * description:
+ */
+public class TokenUtil {
+
+    public String generateToken(Long userId){
+        String token = "token";
+        return token;
+    }
+}
